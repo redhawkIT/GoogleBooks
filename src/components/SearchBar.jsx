fed4dfe8756fcc459fdf7ddb84855970918e0a09
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-12-21 07:04:23 -0600

    create searchBar comp

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,47 @@
+import React, {PropTypes, Component} from 'react'
+import AutoComplete from 'material-ui/AutoComplete'
+import MenuItem from 'material-ui/MenuItem'
+import {browserHistory} from 'react-router'
+
+
+class SearchBar extends Component {
+  state = {
+    text: ''
+  }
+
+  componentWillReceiveProps() {}
+
+  handleSuccessReq(login, name) {
+    const url = `${login}/${name}`
+    this.props.setNavBarTitle(url)
+    this.props.setRepoURL(url)
+    this.props.resetSearch()
+    browserHistory.push(`/${url}`)
+  }
+
+  handleUpdateInput = (value) => this.setState({text: value.slice(0, 50)})
+
+  handleRequest = () => {
+    this.setState({text: ''})
+  }
+
+
+  render() {
+
+    return (
+      <AutoComplete
+        hintText='Search'
+        filter={AutoComplete.fuzzyFilter}
+        dataSource={[1,2,3]}
+        searchText={this.state.text}
+        maxSearchResults={10}
+        onNewRequest={this.handleRequest}
+        onUpdateInput={this.handleUpdateInput}
+      />
+    )
+  }
+}
+
+SearchBar.propTypes = {}
+
+export default SearchBar
