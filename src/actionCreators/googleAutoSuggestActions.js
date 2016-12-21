d3871fa3294dfa8b3dd2a3b2db1f709c09a84dd3
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-12-21 08:40:15 -0600

    base sugguest on books not youtube

--- src/actionCreators/googleAutoSuggestActions.js
+++ src/actionCreators/googleAutoSuggestActions.js
@@ -1,7 +1,10 @@
 import JSONP from 'jsonp'
 import {FETCH_AUTO_SUGGEST_REQUEST, FETCH_AUTO_SUGGEST_FAILURE, FETCH_AUTO_SUGGEST_SUCCESS} from '../constants/actionTypes'
 
-const googleAutoSuggestURL = 'http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&q='
+// client determines the type of result that you receive (XML [toolbar], JSON [firefox], JSONP [chrome])
+// ds limits the search to a specific site (books [bo], youtube [yt] etc.).
+// q is, of course, the query text
+const googleAutoSuggestURL = 'http://suggestqueries.google.com/complete/search?client=books&ds=bo&q='
 
 const request = () => {
   return {
